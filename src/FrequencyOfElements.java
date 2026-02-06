376e5596aee649930955d27274b135ecd1100a52
Virochan V <virochan.tech@gmail.com>
2026-02-06 22:42:23 +0530

    Add Java program to count frequency of elements in an array

---
+++ src/FrequencyOfElements.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class FrequencyOfElements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of array : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter the elements of array : ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        frequencyCount(arr);
+
+    }
+
+    public static void frequencyCount(int[] arr) {
+        boolean[] visited = new boolean[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            if (visited[i]) {
+                continue;
+            }
+            int count = 1;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                    visited[j] = true;
+                }
+
+            }
+            System.out.println(arr[i] + " occurs " + count + " times");
+        }
+    }
+}
