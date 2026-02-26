3e714d854a018a8831086df570f614146442cc82
Virochan V <virochan.tech@gmail.com>
2026-02-26 22:41:28 +0530

    Add Java program to find search insert position using binary search

---
+++ src/arrays/SearchInsertPosition.java
@@ -0,0 +1,33 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class SearchInsertPosition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
+        int res = searchInsertPosition(arr,target);
+        System.out.println(res);
+    }
+
+    static int searchInsertPosition(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return start;
+    }
+}
