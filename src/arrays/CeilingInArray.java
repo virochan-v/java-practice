a14ebe1555aaa4225ed308a423e5b4b8318bca98
Virochan V <virochan.tech@gmail.com>
2026-03-02 18:02:24 +0530

    Add Java programs to find ceiling and floor in a sorted array using binary search

---
+++ src/arrays/CeilingInArray.java
@@ -0,0 +1,36 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class CeilingInArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
+        int ans = ceiling(arr,target);
+        System.out.println("The ceiling of the array : " + ans);
+    }
+
+    static int ceiling(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        if (target > arr[arr.length - 1]) {
+            return -1;
+        }
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return start;
+    }
+}
