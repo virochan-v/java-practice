6081d2358f601221838e3bafe22dcbf69e2ebd65
Virochan V <virochan.tech@gmail.com>
2026-03-10 18:02:26 +0530

    Add Java program to find peak index in a mountain array using binary search

---
+++ src/arrays/PeakIndexInMountainArray.java
@@ -0,0 +1,29 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class PeakIndexInMountainArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int res = peakMountainIndex(arr);
+        System.out.println("The peak element's index is " + res);
+    }
+    static int peakMountainIndex(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid + 1]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+}
