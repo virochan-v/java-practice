3c7ac402e8ce3758df3fe50530e7b6db280f7172
Virochan V <virochan.tech@gmail.com>
2026-04-16 22:31:56 +0530

    Add Java program to find leaders in an array

---
+++ src/arrays/LeadersInArray.java
@@ -0,0 +1,20 @@
+package arrays;
+
+public class LeadersInArray {
+    public static void main(String[] args) {
+        int[] arr = {16,17,4,3,5,2};
+        findLeaders(arr);
+
+    }
+    static void findLeaders(int[] arr) {
+        int n = arr.length;
+        int maxFromRight = arr[n - 1];
+        System.out.print("Leaders : " + maxFromRight + " ");
+        for (int i = n - 2; i >= 0 ; i--) {
+            if (arr[i] > maxFromRight) {
+                maxFromRight = arr[i];
+                System.out.print(maxFromRight + " ");
+            }
+        }
+    }
+}
