e54036967d44e2946779435ef4105332935dd58b
Virochan V <virochan.tech@gmail.com>
2026-04-20 15:34:06 +0530

    Add Java implementation of cyclic sort for arrays containing numbers from 1 to n

---
+++ src/sorting/CyclicSort.java
@@ -0,0 +1,27 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class CyclicSort {
+    public static void main(String[] args) {
+        int[] arr = {3,5,2,1,4};
+        cyclicSort(arr);
+        System.out.println("After Sorting : " + Arrays.toString(arr));
+    }
+    static void cyclicSort(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i] != arr[correct]) {
+                swap(arr,i,correct);
+            } else {
+                i ++;
+            }
+        }
+    }
+    static void swap(int[] arr,int first,int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
