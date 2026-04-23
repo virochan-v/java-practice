d39985be2fe7d16618b32e4fd0417baf3111d7f7
Virochan V <virochan.tech@gmail.com>
2026-04-23 23:06:10 +0530

    Add Java program to find all duplicate numbers in array using cyclic sort

---
+++ src/arrays/FindAllDuplicates.java
@@ -0,0 +1,36 @@
+package arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindAllDuplicates {
+    public static void main(String[] args) {
+        int[] arr = {4,3,2,7,8,2,3,1};
+        List<Integer> ans = findDuplicates(arr);
+        System.out.println("Duplicate Numbers : " + ans);
+
+    }
+    static List<Integer> findDuplicates(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i] != nums[correct]) {
+                swap(nums,i,correct);
+            } else {
+                i ++;
+            }
+        }
+        List<Integer> ans = new ArrayList<>();
+        for (int j = 0;j < nums.length;j++) {
+            if (nums[j] != j + 1) {
+                ans.add(nums[j]);
+            }
+        }
+        return ans;
+    }
+    static void swap(int[] arr,int first,int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
