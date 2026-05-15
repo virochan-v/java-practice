dcf4d43d2466965f7795a5183e499b1bd11e9b53
Virochan V <virochan.tech@gmail.com>
2026-05-15 22:21:15 +0530

    Add Java program to find ASCII value of a character

---
+++ src/strings/ASCIIValue.java
@@ -0,0 +1,13 @@
+package strings;
+
+public class ASCIIValue {
+
+    public static void main(String[] args) {
+
+        char ch = 'A';
+
+        int ascii = (int) ch;
+
+        System.out.println("ASCII Value: " + ascii);
+    }
+}
