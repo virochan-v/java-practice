f1a5b85b128d32ea916d780b05bd949bd4a9346b
Virochan V <virochan.tech@gmail.com>
2026-05-29 22:48:38 +0530

    Add Java program to check if two strings are anagrams

---
+++ src/strings/AnagramCheck.java
@@ -0,0 +1,39 @@
+package strings;
+
+public class AnagramCheck {
+
+    public static void main(String[] args) {
+        String str1 = "listen";
+        String str2 = "silent";
+
+        if (isAnagram(str1, str2)) {
+            System.out.println("Anagram");
+        } else {
+            System.out.println("Not Anagram");
+        }
+    }
+
+    public static boolean isAnagram(String str1, String str2) {
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        int[] freq = new int[26];
+
+        for (int i = 0; i < str1.length(); i++) {
+            freq[str1.charAt(i) - 'a']++;
+            freq[str2.charAt(i) - 'a']--;
+        }
+
+        for (int count : freq) {
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
