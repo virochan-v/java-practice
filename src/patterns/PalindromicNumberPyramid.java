4afc9706bc15dacd6fbb97ec1802b95160a72e22
Virochan V <virochan.tech@gmail.com>
2026-06-06 12:36:21 +0530

    Add Java program to print palindromic number pyramid pattern

---
+++ src/patterns/PalindromicNumberPyramid.java
@@ -0,0 +1,21 @@
+package patterns;
+
+public class PalindromicNumberPyramid {
+    public static void main(String[] args) {
+        palindromicNumberPyramid(5);
+    }
+    static void palindromicNumberPyramid(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int space = 1; space <= n - row; space++) {
+                System.out.print("  ");
+            }
+            for (int col = row; col >= 1; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <= row; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
+}
