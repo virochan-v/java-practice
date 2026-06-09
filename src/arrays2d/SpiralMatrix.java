9a0e3d45340d537420db3b33e06aa02339989d18
Virochan V <virochan.tech@gmail.com>
2026-06-09 21:42:47 +0530

    Add Java program to traverse matrix in spiral order

---
+++ src/arrays2d/SpiralMatrix.java
@@ -0,0 +1,53 @@
+
+package arrays2d;
+
+public class SpiralMatrix {
+
+    public static void printSpiral(int[][] matrix) {
+        int top = 0;
+        int bottom = matrix.length - 1;
+        int left = 0;
+        int right = matrix[0].length - 1;
+
+        while (top <= bottom && left <= right) {
+
+            // Left to Right
+            for (int i = left; i <= right; i++) {
+                System.out.print(matrix[top][i] + " ");
+            }
+            top++;
+
+            // Top to Bottom
+            for (int i = top; i <= bottom; i++) {
+                System.out.print(matrix[i][right] + " ");
+            }
+            right--;
+
+            // Right to Left
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    System.out.print(matrix[bottom][i] + " ");
+                }
+                bottom--;
+            }
+
+            // Bottom to Top
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    System.out.print(matrix[i][left] + " ");
+                }
+                left++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        printSpiral(matrix);
+    }
+}
