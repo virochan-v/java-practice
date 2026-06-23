6cbec0369de001ace45fe7fb88bc6569c0c51ae7
Virochan V <virochan.tech@gmail.com>
2026-06-23 17:08:14 +0530

    refactor: replace hardcoded values with variables

--- src/bitmanipulation/ResetIthBit.java
+++ src/bitmanipulation/ResetIthBit.java
@@ -4,7 +4,7 @@ public class ResetIthBit {
     public static void main(String[] args) {
         int n = 13;
         int i = 2;
-        System.out.println("The new number : " + resetIthBit(13,2));
+        System.out.println("The new number : " + resetIthBit(n,i));
     }
     static int resetIthBit(int n, int i) {
         return n & ~ (1 << i);
