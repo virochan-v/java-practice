9c1ccb36dddcd13a3ec832a366a32f9269d44263
Virochan V <virochan.tech@gmail.com>
2026-06-27 13:52:00 +0530

    Add Java program to find sum of nth row in Pascal's triangle using bit manipulation

---
+++ src/bitmanipulation/PascalTriangleRowSum.java
@@ -0,0 +1,9 @@
+package bitmanipulation;
+
+public class PascalTriangleRowSum {
+    public static void main(String[] args) {
+        int row = 4;
+        int sum = 1 << row;
+        System.out.println("The sum of row : " + sum);
+    }
+}
