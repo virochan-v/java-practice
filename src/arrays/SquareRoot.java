f6aa122fc61523e6946d4a9e9007ad7d77204272
Virochan V <virochan.tech@gmail.com>
2026-07-01 12:29:50 +0530

    Add Java program to find square root of a number with decimal precision using binary search

---
+++ src/arrays/SquareRoot.java
@@ -0,0 +1,33 @@
+package arrays;
+
+public class SquareRoot {
+    public static void main(String[] args) {
+        int n = 40;
+        int p = 3;
+        System.out.printf("The Square Root : " + sqrt(n, p));
+    }
+    static double sqrt(int n, int p) {
+        int start = 0;
+        int end = n;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (mid * mid == n) {
+                return mid;
+            } else if (mid * mid > n) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        double root = end;
+        double increment = 0.1;
+        for (int i = 0; i < p; i++) {
+            while (root * root <= n) {
+                root += increment;
+            }
+            root -= increment;
+            increment /= 10;
+        }
+        return root;
+    }
+}
